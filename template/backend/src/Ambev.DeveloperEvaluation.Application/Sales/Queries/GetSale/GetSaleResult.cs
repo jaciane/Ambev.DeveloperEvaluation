c99a61dc2896826360beae503926745b15298835
Jaciane <jacianeba@gmail.com>
2025-04-17 15:20:49 -0300

    Add use case Get Sale with handle, result, validator and mapper

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/Queries/GetSale/GetSaleResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.Queries.GetSale
+{
+    public class GetSaleResult
+    {
+        public string SaleNumber { get; set; } = string.Empty;
+        public DateTime SaleDate { get; set; }
+
+        public string CustomerName { get; set; } = string.Empty;
+        public string BranchName { get; set; } = string.Empty;
+
+        public decimal TotalAmount { get; set; }
+        public bool IsCanceled { get; set; }
+
+        public List<GetSaleItemDto> Items { get; set; } = new();
+    }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/Queries/GetSale/GetSaleValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.Queries.GetSale
+{
+    /// <summary>
+    /// Validator for GetSaleCommand
+    /// </summary>
+    public class GetSaleValidator : AbstractValidator<GetSaleQuery>
+    {
+
+        /// <summary>
+        /// Initializes validation rules for GetSaleCommand
+        /// </summary>
+        public GetSaleValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty()
+                .WithMessage("Sale ID is required");
+        }
+    }
+}
