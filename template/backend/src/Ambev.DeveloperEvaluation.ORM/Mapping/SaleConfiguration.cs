d56d221fe35b4d86c9c11ff43cc870ca7ee1d5c6
Jaciane <jacianeba@gmail.com>
2025-04-17 15:06:54 -0300

    Map data models to the database

---
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Mapping/SaleConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping
+{
+    public class SaleConfiguration : IEntityTypeConfiguration<Sale>
+    {
+        public void Configure(EntityTypeBuilder<Sale> builder)
+        {
+            builder.ToTable("Sales");
+
+            builder.HasKey(s => s.Id);
+
+            builder.Property(s => s.SaleNumber)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(s => s.CustomerId)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(s => s.CustomerName)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            builder.Property(s => s.BranchId)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(s => s.BranchName)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            builder.Property(s => s.SaleDate)
+                .IsRequired();
+
+            builder.Property(s => s.IsCanceled)
+                .IsRequired();
+
+            // Relations:: 1 Sale -> N SaleItems
+            builder.HasMany(s => s.Items)
+                .WithOne() // No reverse navigation property
+                .HasForeignKey("SaleId")
+                .OnDelete(DeleteBehavior.Cascade); // When the sale is deleted, the items are also deleted.
+        }
+    }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Mapping/SaleItemConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping
+{
+    public class SaleItemConfiguration : IEntityTypeConfiguration<SaleItem>
+    {
+        public void Configure(EntityTypeBuilder<SaleItem> builder)
+        {
+            builder.ToTable("SaleItems");
+
+            builder.HasKey(i => i.Id);
+
+            builder.Property(i => i.ProductId)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(i => i.ProductName)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            builder.Property(i => i.Quantity)
+                .IsRequired();
+
+            builder.Property(i => i.UnitPrice)
+                .IsRequired()
+                .HasColumnType("decimal(18,2)");
+
+            builder.Property(i => i.Discount)
+                .IsRequired()
+                .HasColumnType("decimal(18,2)");
+
+            builder.Property(i => i.IsCanceled)
+                .IsRequired();
+
+            // Relations with Sale (via shadow property "SaleId")
+            builder.Property<Guid>("SaleId"); // Shadow property to FK (if not in class)
+        }
+    }
+}
