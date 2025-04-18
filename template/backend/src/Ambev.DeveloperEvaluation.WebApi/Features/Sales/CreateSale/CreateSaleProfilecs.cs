1d80f262c683430c31cee3724f4c36fea0c8845f
Jaciane <jacianeba@gmail.com>
2025-04-18 00:02:06 -0300

    Add feature CreateSale including request, response, validator and profile

---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/CreateSale/CreateSaleProfilecs.cs
@@ -0,0 +1,25 @@
+﻿using Ambev.DeveloperEvaluation.Application.Sales.Commands.CreateSale;
+using Ambev.DeveloperEvaluation.Application.Users.CreateUser;
+using Ambev.DeveloperEvaluation.WebApi.Features.Users.CreateUser;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales.CreateSale
+{
+    /// <summary>
+    /// Profile for mapping between Application and API CreateSale responses
+    /// </summary>
+    public class CreateSaleProfilecs : Profile
+    {
+        /// <summary>
+        /// Initializes the mappings for CreateSale feature
+        /// </summary>
+        public CreateSaleProfilecs()
+        {
+            CreateMap<CreateSaleRequest, CreateSaleCommand>();
+            CreateMap<CreateSaleItemRequest, CreateSaleItem>();
+            CreateMap<CreateSaleResult, CreateSaleResponse>();
+            CreateMap<CreateSaleItemResult, CreateSaleItemResponse>();
+
+        }
+    }
+}
--- template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/SalesController.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/SalesController.cs
@@ -1,5 +1,7 @@
-﻿using Ambev.DeveloperEvaluation.Application.Sales.Queries.GetSale;
+﻿using Ambev.DeveloperEvaluation.Application.Sales.Commands.CreateSale;
+using Ambev.DeveloperEvaluation.Application.Sales.Queries.GetSale;
 using Ambev.DeveloperEvaluation.WebApi.Common;
+using Ambev.DeveloperEvaluation.WebApi.Features.Sales.CreateSale;
 using Ambev.DeveloperEvaluation.WebApi.Features.Sales.GetSale;
 using AutoMapper;
 using MediatR;
@@ -55,12 +57,40 @@ namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales
             {
                 return NotFound(new ApiResponse { Message = ex.Message });
             }
-            catch (Exception ex)
+            catch (Exception )
             {
                 return StatusCode(500, new ApiResponse { Message = "An unexpected error occurred" });
             }
         }
 
-        
+        /// <summary>
+        /// Handles the creation of a new sale.
+        /// </summary>
+        /// <param name="request">The sale data to create.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>The created sale details.</returns>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> CreateSale([FromBody] CreateSaleRequest request, CancellationToken cancellationToken)
+        {
+            var validator = new CreateSaleRequestValidator();
+            var validationResult = await validator.ValidateAsync(request, cancellationToken);
+
+            if (!validationResult.IsValid)
+                return BadRequest(validationResult.Errors);
+
+            var command = _mapper.Map<CreateSaleCommand>(request);
+            var response = await _mediator.Send(command, cancellationToken);
+
+            return Created(string.Empty, new ApiResponseWithData<CreateSaleResponse>
+            {
+                Success = true,
+                Message = "User created successfully",
+                Data = _mapper.Map<CreateSaleResponse>(response)
+            });
+        }
+
+
     }
 }
